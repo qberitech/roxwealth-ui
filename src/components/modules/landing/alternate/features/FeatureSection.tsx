1cedfeb130869067208e26af8aa165f0512e954b
Rohan-2107 <rohanrkc289@gmail.com>
2024-02-02 17:51:06 +0530

    no change

--- src/components/modules/landing/alternate/features/FeatureSection.tsx
+++ src/components/modules/landing/alternate/features/FeatureSection.tsx
@@ -1,7 +1,6 @@
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
-import Button from 'components/base/Button';
 import { Feature } from 'data/landing/alternate-landing-data';
 import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
